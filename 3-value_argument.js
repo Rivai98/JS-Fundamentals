0645a0e5ad8ef28d80708044570ba87185e50a4f
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-13 21:51:35 +0300

    Fixing some bugs in Task 03

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,6 +2,8 @@ let arg = process.argv ;
 if(arg[2]== null ){
     console.log("No argument"); 
 }else{
-    console.log(arg); 
+    for(i = 2 ; arg[i]!= null ; i++){
+        console.log(arg[i]); 
+    }
 
 }
