5cf87b42d87ee5f9f479f7d22cdf8bf77bce0a2c
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-13 20:54:09 +0300

    Add Missed Casese to correctly solve Task 02 Arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,6 +3,8 @@ let arg = process.argv;
 if(arg.length <= 2 ){ 
     console.log("No argument"); 
 
-}else {
+}else if (arg.length > 2  && arg.length <=3  ) {
     console.log("Argument found"); 
+}else{
+    console.log("Arguments found"); 
 }
