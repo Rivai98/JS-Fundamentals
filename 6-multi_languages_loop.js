42da5dca6c729fc83aa818f8f213b4d3f5a842ce
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-09-14 13:02:06 +0300

    Task 6 Trying to Fix Errors

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,3 @@
 let arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for(counter = 0 ; counter < arr.length ; counter++ ){
-    console.log(arr[counter]); 
-}
+for(counter = 0 ; counter < arr.length ; counter++ ) console.log(arr[counter]); 
+
